5edbfce8dcc970b20568e0d1a1ab6a9a9a5c3226
Ashima Shrivastava <ashima206@gmail.com>
2016-11-28 15:42:20 -0500

    added access token property on user

--- app/Http/Controllers/UserController.js
+++ app/Http/Controllers/UserController.js
@@ -18,9 +18,11 @@ class UserController {
 
 	*login(request,response){
 		let data = request.only('userName','password')
+		let user = yield User.findBy('userName',data.userName)
 		try{
 			const token = yield request.auth.attempt(data.userName,data.password)
-			response.status(200).send(token)
+			user.access_token = token
+			response.status(200).send(user)
 		} catch(e){
 			response.status(401).unauthorized({error: e.message})
 		}
