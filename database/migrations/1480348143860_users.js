0e6e72b9a1ce0f2ba25547b564edc627d3a91e2f
Ashima Shrivastava <ashima206@gmail.com>
2016-11-28 10:56:09 -0500

    created users table

---
+++ database/migrations/1480348143860_users.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const Schema = use('Schema')
+
+class UsersTableSchema extends Schema {
+
+  up () {
+    this.create('users', (table) => {
+      table.increments()
+      table.timestamps()
+      table.string('firstName')
+      table.string('lastName')
+      table.string('email').unique()
+      table.string('userName').unique()
+      table.string('password',40)
+
+    })
+  }
+
+  down () {
+    this.drop('users')
+  }
+
+}
+
+module.exports = UsersTableSchema
